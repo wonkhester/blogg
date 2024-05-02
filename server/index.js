f4a280f754b88129f9ca0a5afe46f8afca437d2a
wonkhester <kevinradley@outlook.com>
2024-05-02 20:26:19 +0200

    now possible to create new bloggs

--- server/index.js
+++ server/index.js
@@ -6,12 +6,13 @@ import path from "path";
 import { File } from "buffer";
 import bcrypt from "bcrypt";
 
-//import login from "login.js";
-//import signup from "signup.js";
-
 const app = express();
 const port = 3000;
 const __dirname = path.resolve();
+const dataBasePath = path.join(__dirname, "..", "db");
+const blogPath = path.join(dataBasePath, "blogs");
+const blogContentPath = path.join(dataBasePath, "blogContent");
+const accountsPath = path.join(dataBasePath, "accounts");
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,26 +22,23 @@ app.use(session({
     saveUninitialized: true
 }));
 
-console.log("__dirname:", __dirname);
 
-
-async function blogs() {
+export async function blogs() {
     try {
-        const directoryPath = path.join(__dirname, "..", "db", "blogs");
-        const files = await fs.readdir(directoryPath);
+        const files = await fs.readdir(blogPath);
         const blogDataPromises = files.map(async fileName => {
             try {
-                const file = await fs.readFile(path.join(directoryPath, fileName), "utf8");
+                const file = await fs.readFile(path.join(blogPath, fileName), "utf8");
                 if (!file) {
                     throw new Error(`File "${fileName}" is empty or could not be read.`);
                 }
                 const fileLines = file.split("\n");
 
-                const content = fileLines.slice(4).join("\n"); // Join lines starting from index 4
+                const preview = fileLines.slice(4).join("\n");
                 
                 const postedStr = () => {
-                    const postedTimestamp = parseInt(fileLines[2]); // Parse the timestamp string to an integer
-                    const postedDate = new Date(postedTimestamp); // Create a Date object from the timestamp
+                    const postedTimestamp = parseInt(fileLines[2]);
+                    const postedDate = new Date(postedTimestamp);
                     if (isNaN(postedDate.getTime())) {
                         return "Error";
                     }
@@ -63,13 +61,17 @@ async function blogs() {
                     }
                 }
 
+                if (postedStr() === "Error") {
+                    return null;
+                }
+
                 const fileData = {
                     id: fileName.split(".")[0],
                     title: fileLines[0],
                     author: fileLines[1],
                     posted: postedStr(),
                     tags: fileLines[3].split(" "),
-                    content: content
+                    preview: preview
                 }
 
                 return fileData;
@@ -78,30 +80,33 @@ async function blogs() {
                 return null;
             }
         });
-        return Promise.all(blogDataPromises);
+        
+        const blogData = await Promise.all(blogDataPromises);
+        return blogData.filter(data => data !== null);
     } catch (error) {
         console.error("Error reading directory:", error);
         return [];
     }
 };
 
-async function userBlogs() {
+export async function userBlogs(ownedBlogs) {
     try {
-        const directoryPath = path.join(__dirname, "..", "db", "blogs");
-        const files = await fs.readdir(directoryPath);
+        const files = ownedBlogs.map(blogId => {
+            return blogId + ".db";
+        });
         const blogDataPromises = files.map(async fileName => {
             try {
-                const file = await fs.readFile(path.join(directoryPath, fileName), "utf8");
+                const file = await fs.readFile(path.join(blogPath, fileName), "utf8");
                 if (!file) {
                     throw new Error(`File "${fileName}" is empty or could not be read.`);
                 }
                 const fileLines = file.split("\n");
 
-                const content = fileLines.slice(4).join("\n"); // Join lines starting from index 4
+                const preview = fileLines.slice(4).join("\n");
                 
                 const postedStr = () => {
-                    const postedTimestamp = parseInt(fileLines[2]); // Parse the timestamp string to an integer
-                    const postedDate = new Date(postedTimestamp); // Create a Date object from the timestamp
+                    const postedTimestamp = parseInt(fileLines[2]);
+                    const postedDate = new Date(postedTimestamp);
                     if (isNaN(postedDate.getTime())) {
                         return "Error";
                     }
@@ -127,10 +132,11 @@ async function userBlogs() {
                 const fileData = {
                     id: fileName.split(".")[0],
                     title: fileLines[0],
-                    author: fileLines[1],
+                    author: "you",
                     posted: postedStr(),
                     tags: fileLines[3].split(" "),
-                    content: content
+                    preview: preview,
+                    owner: true,
                 }
 
                 return fileData;
@@ -146,24 +152,135 @@ async function userBlogs() {
     }
 }
 
-app.get("/blog/:data", (req, res) => {
+app.get("/blog/edit/:id", async (req, res) => {
+    const id = req.params.id;
+    try {
+        if (req.session && req.session.isLoggedIn) {
+            const username = req.session.username;
 
+            const userFilePath = path.join(accountsPath, `${username}.json`);
+            const userData = JSON.parse(await fs.readFile(userFilePath, "utf8"));
+            
+            if (userData.ownedBlogs.includes(parseInt(id))) {
+                const MainFile = await fs.readFile(path.join(blogPath, id + ".db"), "utf8");
+                if (!MainFile) {
+                    throw new Error(`File "${id}.db" is empty or could not be read.`);
+                }
+                const MainFileLines = MainFile.split("\n");
+
+                const preview = MainFileLines.slice(4).join("\n");
+
+                async function getBlogList () {
+                    const blogfiles = await fs.readdir(path.join(blogContentPath, `${id}`));
+                    
+                }
+
+                const blogData = {
+                    id: id,
+                    title: MainFileLines[0],
+                    tags: MainFileLines[3].split(" "),
+                    preview: preview,
+                    blogList: /*await getBlogList()*/ null,
+                }
+                res.render("blog/edit.ejs", {blogData: blogData});
+            }
+            else {
+                res.redirect("/home");
+            }
+        } else {
+            res.redirect("/login");
+        }
+    }
+    catch (error) {
+        console.log(error)
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+app.get("/blog/new", async (req, res) => {
+    res.render("blog/new.ejs");
+});
+
+app.post("/blog/new", async (req, res) => {
+    const {title} = req.body;
+
+    if (title !== "") {
+        try {
+            if (req.session && req.session.isLoggedIn) {
+                const username = req.session.username;
+    
+                const userFilePath = path.join(accountsPath, `${username}.json`);
+                const userData = JSON.parse(await fs.readFile(userFilePath, "utf8"));
+    
+                const files = await fs.readdir(blogPath);
+                const numbers = files.filter(file => /^\d+\.db$/.test(file)).map(file => parseInt(file.split('.')[0]));
+                
+                let highestNumber = 0;
+                if (numbers.length > 0) {
+                    highestNumber = Math.max(...numbers);
+                }
+    
+                const newFileName = highestNumber + 1;
+                const fileContent = title+ "\n" + username + "\n\n\n";
+    
+                await fs.writeFile(path.join(blogPath, `${newFileName}.db`), fileContent);
+                await fs.mkdir(path.join(blogContentPath, `${newFileName}`));
+    
+                userData.ownedBlogs.push(newFileName)
+                await fs.writeFile(path.join(accountsPath, `${username}.json`), JSON.stringify(userData));
+    
+                res.redirect(`/blog/edit/${newFileName}`);
+            } else {
+                res.redirect("/login");
+            }
+        } catch (error) {
+            console.error("Error creating new blog post:", error);
+            res.status(500).send("Internal Server Error");
+        }
+    }
+    else {
+        res.render("blog/new.ejs");
+    }
+});
+
+app.get("/blog/:id", async (req, res) => {
+    const id = req.params.id;
+    try {
+        const MainFile = await fs.readFile(path.join(blogPath, id + ".db"), "utf8");
+        if (!MainFile) {
+            throw new Error(`File "${id}.db" is empty or could not be read.`);
+        }
+        const MainFileLines = MainFile.split("\n");
+
+        const fileData = {
+            id: id,
+            title: fileLines[0],
+            author: "you",
+            posted: postedStr(),
+            tags: fileLines[3].split(" "),
+            preview: preview
+        }
+        res.render("home/index.ejs", { blogs: blogData, activeFilters: {}, blogSearch: ""});
+    }
+    catch (error) {
+        console.log(error)
+        res.status(500).send("Internal Server Error");
+    }
 });
 
 app.get("/home", async (req, res) => {
     try {
         if (req.session && req.session.isLoggedIn) {
             const username = req.session.username;
-            const userFilePath = path.join(__dirname, "..", "db", "accounts", username + ".json");
+            const userFilePath = path.join(accountsPath, username + ".json");
     
             const fileData = await fs.readFile(userFilePath, "utf8");
             const userData = JSON.parse(fileData);
     
             const ownedBlogs = userData.ownedBlogs;
+            const blogData = await userBlogs(ownedBlogs);
     
-            console.log(ownedBlogs)
-    
-            res.render("home/main.ejs");
+            res.render("home/index.ejs", { blogs: blogData, activeFilters: {}, blogSearch: ""});
         } else {
             res.redirect("/login");
         }
@@ -180,8 +297,7 @@ app.get("/login", (req, res) => {
 
 app.post("/login", async (req, res) => {
     const { username, password } = req.body;
-    const directoryPath = path.join(__dirname, "..", "db", "accounts");
-    const filePath = path.join(directoryPath, `${username}.json`);
+    const filePath = path.join(accountsPath, `${username}.json`);
 
     try {
         const userData = await fs.readFile(filePath, "utf8");
@@ -208,8 +324,7 @@ app.get("/signup", (req, res) => {
 
 app.post("/signup", async (req, res) => {
     const { username, password } = req.body;
-    const directoryPath = path.join(__dirname, "..", "db", "accounts");
-    const filePath = path.join(directoryPath, `${username}.json`);
+    const filePath = path.join(accountsPath, `${username}.json`);
 
     try {
         const fileExists = await fs.access(filePath).then(() => true).catch(() => false);
@@ -265,11 +380,9 @@ app.get("/:data", (req, res) => {
 });
 
 app.get("/", async (req, res) => {
-    const activeFilters = {};
-    const blogSearch = "";
     try {
         const blogData = await blogs();
-        res.render("list/index.ejs", { blogs: blogData, activeFilters: activeFilters, blogSearch: blogSearch});
+        res.render("list/index.ejs", { blogs: blogData, activeFilters: {}, blogSearch: ""});
     } catch (error) {
         console.error("Error rendering view:", error);
         res.status(500).send("Internal Server Error");
